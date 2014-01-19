9ba66c0680dfc247cbd8dc5657a45bfa5a8cac5e
noor <syed.noor@outlook.com>
2014-01-19 21:48:59 +0530

    included app project

---
+++ Eqstra.VehicleInspection/App.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Practices.Prism.StoreApps;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Microsoft.Practices.Unity;
+
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace Eqstra.VehicleInspection
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : MvvmAppBase
+    {
+        private readonly IUnityContainer _container = new UnityContainer();
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+
+        }
+
+
+        protected override Task OnLaunchApplication(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Login", args.Arguments);
+            Window.Current.Activate();
+            return Task.FromResult<object>(null);
+        }
+
+        protected override void OnInitialize(IActivatedEventArgs args)
+        {
+            base.OnInitialize(args);
+            _container.RegisterInstance(NavigationService);
+            _container.RegisterInstance(SessionStateService);
+            ViewModelLocator.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewModelTypeName = string.Format(CultureInfo.InvariantCulture, "Eqstra.VehicleInspection.UILogic.ViewModels.{0}ViewModel,Eqstra.VehicleInspection.UILogic,Version 1.0.0.0, Culture=neutral", viewType.Name);
+                return Type.GetType(viewModelTypeName);
+            });           
+        }
+
+        protected override object Resolve(Type type)
+        {
+            return _container.Resolve(type);
+        }
+    }
+}
---
+++ Eqstra.VehicleInspection/Views/DrivingDirectionPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Bing.Maps;
+using Microsoft.Practices.Prism.StoreApps;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Eqstra.VehicleInspection.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DrivingDirectionPage : VisualStateAwarePage
+    {
+        readonly Geolocator geolocator = new Geolocator();
+        Pushpin pushpin;
+        public DrivingDirectionPage()
+        {
+            this.InitializeComponent();
+            pushpin = new Pushpin{Visibility = Windows.UI.Xaml.Visibility.Collapsed};
+            this.MyMap.Children.Add(pushpin);
+            geolocator.PositionChanged += geolocator_PositionChanged;
+        }
+
+        async void geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,new DispatchedHandler(()=>
+            {
+                var location = new Location(args.Position.Coordinate.Latitude,args.Position.Coordinate.Longitude);
+                
+                MapLayer.SetPosition(pushpin,location);
+                pushpin.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                MyMap.SetView(location,16);
+            }));
+        }
+    }
+}
