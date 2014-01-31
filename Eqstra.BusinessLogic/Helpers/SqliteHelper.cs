1bd7fff9b082c1f0988af829aea8c55263a305e5
noor <syed.noor@mazikglobal.com>
2014-01-31 20:28:36 +0530

    added business logic proje

---
+++ Eqstra.BusinessLogic/Helpers/SqliteHelper.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Practices.Prism.StoreApps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Eqstra.BusinessLogic.Helpers
+{
+    public sealed class SqliteHelper
+    {
+        public SqliteHelper()
+        {
+
+
+        }
+
+        private static readonly SqliteHelper instance = new SqliteHelper();
+
+        public static SqliteHelper Instance
+        {
+            get { return instance; }
+
+        }
+
+        private SQLite.SQLiteAsyncConnection connection;
+
+        public SQLite.SQLiteAsyncConnection Connection
+        {
+            get { return connection; }
+            set { connection = value; }
+        }
+
+        public async void ConnectionDatabaseAsync()
+        {
+            if (connection == null)
+            {
+                var db = await ApplicationData.Current.RoamingFolder.GetFileAsync("SQLiteDB\\eqstramobility.sqlite");
+                connection = new SQLite.SQLiteAsyncConnection(db.Path);
+            }
+        }
+
+        public async void DropTable<T>() where T : new()
+        {
+
+            await this.Connection.DropTableAsync<T>();
+        }
+
+        public Task<List<T>> LoadTableAsync<T>()
+            where T : ValidatableBindableBase,new()
+            
+        {
+            return this.Connection.Table<T>().ToListAsync();
+        }
+
+        public async void InsertAllAsync<T>(IEnumerable<T> items) where T : new()
+        {
+            await this.Connection.InsertAllAsync(items);
+        }
+
+
+    }
+}
---
+++ Eqstra.BusinessLogic/Task.cs
@@ -0,0 +1,82 @@
+﻿using Eqstra.BusinessLogic.Enums;
+using Microsoft.Practices.Prism.StoreApps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eqstra.BusinessLogic
+{
+   public class Task : ValidatableBindableBase
+    {
+        private string caseNumber;
+
+        public string CaseNumber
+        {
+            get { return caseNumber; }
+            set { caseNumber = value; }
+        }
+
+        private long cellNumber;
+
+        public long CellNumber
+        {
+            get { return cellNumber; }
+            set { SetProperty(ref cellNumber, value); }
+        }
+
+        private DateTime statusDueDate;
+
+        public DateTime StatusDueDate
+        {
+            get { return statusDueDate; }
+            set { SetProperty(ref statusDueDate, value); }
+        }
+
+        private TaskStatusEnum status;
+
+        public TaskStatusEnum Status
+        {
+            get { return status; }
+            set { SetProperty(ref status, value); }
+        }
+
+        private string allocatedTo;
+
+        public string AllocatedTo
+        {
+            get { return allocatedTo; }
+            set { SetProperty(ref allocatedTo, value); }
+        }
+
+        private DateTime confirmedDateTime;
+
+        public DateTime ConfirmedDateTime
+        {
+            get { return confirmedDateTime; }
+            set { SetProperty(ref confirmedDateTime, value); }
+        }
+
+        private string customerId;
+
+        public string CustomerId
+        {
+            get { return customerId; }
+            set { SetProperty(ref customerId, value); }
+        }
+
+        private string registrationNumber;
+
+        public string RegistrationNumber
+        {
+            get { return registrationNumber; }
+            set { SetProperty(ref registrationNumber, value); }
+        }
+
+
+      
+      
+
+    }
+}
