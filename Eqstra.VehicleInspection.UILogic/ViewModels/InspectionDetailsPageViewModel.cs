7472e4ea0383aa049e97978a6dea94e4608139bc
noor <syed.noor@mazikglobal.com>
2014-02-01 20:27:50 +0530

    Customer,

---
+++ Eqstra.VehicleInspection.UILogic/ViewModels/InspectionDetailsPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Eqstra.BusinessLogic;
+using Eqstra.BusinessLogic.Helpers;
+using Microsoft.Practices.Prism.StoreApps;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Eqstra.VehicleInspection.UILogic.ViewModels
+{
+    public class InspectionDetailsPageViewModel : ViewModel
+    {
+        private ObservableCollection<Eqstra.BusinessLogic.Task> inspectionList;
+
+        public ObservableCollection<Eqstra.BusinessLogic.Task> InspectionList
+        {
+            get { return inspectionList; }
+            set { SetProperty(ref inspectionList, value); }
+        }
+
+        public InspectionDetailsPageViewModel()
+        {
+            this.inspectionList = new ObservableCollection<BusinessLogic.Task>();
+        }
+        async public override void OnNavigatedTo(object navigationParameter, Windows.UI.Xaml.Navigation.NavigationMode navigationMode, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(navigationParameter, navigationMode, viewModelState);
+            var list = await SqliteHelper.Storage.LoadTableAsync<Eqstra.BusinessLogic.Task>();
+            foreach (Eqstra.BusinessLogic.Task item in list)
+            {
+                var cust = await SqliteHelper.Storage.GetSingleRecordAsync<Customer>(x => x.Id == item.CustomerId);
+                item.CustomerName = cust.Name;
+                item.Address = cust.Address;
+                this.inspectionList.Add(item);
+            }
+        }
+
+        public override void OnNavigatedFrom(Dictionary<string, object> viewModelState, bool suspending)
+        {
+            base.OnNavigatedFrom(viewModelState, suspending);
+        }
+    }
+}
