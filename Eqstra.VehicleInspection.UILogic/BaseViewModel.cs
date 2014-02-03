829705f316c88449699df768842d75265dd63a46
noor <syed.noor@mazikglobal.com>
2014-02-03 19:51:29 +0530

    views added TrimInterior, Bodywork, Glass

---
+++ Eqstra.VehicleInspection.UILogic/BaseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Eqstra.BusinessLogic;
+using Microsoft.Practices.Prism.StoreApps;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Capture;
+
+namespace Eqstra.VehicleInspection.UILogic
+{
+   public class BaseViewModel:ViewModel
+    {
+       public BaseViewModel()
+       {
+           TakeSnapshotCommand = DelegateCommand<ObservableCollection<ImageCapture>>.FromAsyncHandler(async (param) =>
+           {
+               await TakeSnapshotAsync(param);
+           });
+       }
+        public Object Model { get; set; }
+        public DelegateCommand<ObservableCollection<ImageCapture>> TakeSnapshotCommand { get; set; }
+        protected async System.Threading.Tasks.Task TakeSnapshotAsync<T>(T list) where T : ObservableCollection<ImageCapture>
+        {
+            CameraCaptureUI ccui = new CameraCaptureUI();
+            var file = await ccui.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            if (file != null)
+            {
+                list.Add(new ImageCapture { ImagePath = file.Path });
+            }
+        } 
+    }
+}
---
+++ Eqstra.VehicleInspection.UILogic/ViewModels/Passenger/BodyworkUserControlViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eqstra.VehicleInspection.UILogic.ViewModels
+{
+    public class BodyworkUserControlViewModel : BaseViewModel
+    {
+
+    }
+}
