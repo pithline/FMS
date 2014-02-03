cc8b730e6616d749b1458c36682218617cabff5e
noor <syed.noor@mazikglobal.com>
2014-02-03 12:16:17 +0530

    inspection history, vehicledetails,passenger vehicle inpection pages included

---
+++ Eqstra.BusinessLogic/InspectionHistory.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Practices.Prism.StoreApps;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eqstra.BusinessLogic
+{
+   public class InspectionHistory : ValidatableBindableBase
+    {
+        private string id;
+       [PrimaryKey,AutoIncrement,Indexed]
+        public string Id
+        {
+            get { return id; }
+            set { SetProperty(ref id, value); }
+        }
+
+        private string customerId;
+
+        public string CustomerId
+        {
+            get { return customerId; }
+            set { SetProperty(ref customerId, value); }
+        }
+
+        private DateTime inspectedOn;
+
+        public DateTime InspectedOn
+        {
+            get { return inspectedOn; }
+            set { SetProperty(ref inspectedOn, value); }
+        }
+
+        private string inspectedBy;
+
+        public string InspectedBy
+        {
+            get { return inspectedBy; }
+            set { SetProperty(ref inspectedBy, value); }
+        }
+
+        private string registrationNumber;
+
+        public string RegistrationNumber
+        {
+            get { return registrationNumber; }
+            set { registrationNumber = value; }
+        }
+
+        private List<string> inspectionResult;
+
+        public List<string> InspectionResult
+        {
+            get { return inspectionResult; }
+            set { SetProperty(ref inspectionResult, value); }
+        }
+
+    }
+}
