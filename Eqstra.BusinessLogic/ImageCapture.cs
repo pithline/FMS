1db498037bbc5b9526dfdefd9ac654d455434ac3
noor <syed.noor@mazikglobal.com>
2014-02-04 11:44:43 +0530

    untracted files commit

---
+++ Eqstra.BusinessLogic/ImageCapture.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Prism.StoreApps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Eqstra.BusinessLogic
+{
+   public class ImageCapture : ValidatableBindableBase
+    {
+       
+        private string imagePath;
+
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set { SetProperty(ref imagePath, value); }
+        }
+
+
+        
+    }
+}
---
+++ Eqstra.VehicleInspection.UILogic/ViewModels/Passenger/TrimIntUserControlViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Eqstra.BusinessLogic;
+using Eqstra.BusinessLogic.Passenger;
+using Microsoft.Practices.Prism.StoreApps;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Capture;
+
+namespace Eqstra.VehicleInspection.UILogic.ViewModels
+{
+    public class TrimIntUserControlViewModel : BaseViewModel
+    {
+        public TrimIntUserControlViewModel()
+        {
+            this.Model = new TrimInterior();            
+        }
+
+       
+
+
+
+
+    }
+}
