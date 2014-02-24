ed332a71e359fb8dd467b2c0d2c01f2fc6a2cf3d
noor <syed.noor@mazikglobal.com>
2014-02-24 17:42:42 +0530

    passenger vehicle inspection data bound

--- Eqstra.VehicleInspection.UILogic/BaseViewModel.cs
+++ Eqstra.VehicleInspection.UILogic/BaseViewModel.cs
@@ -10,25 +10,63 @@ using Windows.Media.Capture;
 
 namespace Eqstra.VehicleInspection.UILogic
 {
-   public class BaseViewModel:ViewModel
+    public class BaseViewModel : ViewModel
     {
-       public BaseViewModel()
-       {
-           TakeSnapshotCommand = DelegateCommand<ObservableCollection<ImageCapture>>.FromAsyncHandler(async (param) =>
-           {
-               await TakeSnapshotAsync(param);
-           });
-       }
-        public Object Model { get; set; }
+        public BaseViewModel()
+        {
+            TakeSnapshotCommand = DelegateCommand<ObservableCollection<ImageCapture>>.FromAsyncHandler(async (param) =>
+            {
+                await TakeSnapshotAsync(param);
+            });
+
+            TakePictureCommand = DelegateCommand<ImageCapture>.FromAsyncHandler(async (param) =>
+                {
+                    await TakePictureAsync(param);
+                });
+        }
+
+        async public System.Threading.Tasks.Task TakePictureAsync(ImageCapture param)
+        {
+            try
+            {
+                CameraCaptureUI cam = new CameraCaptureUI();
+                var file = await cam.CaptureFileAsync(CameraCaptureUIMode.Photo);
+                if (file != null)
+                {
+                    param.ImagePath = file.Path;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        private Object model;
+
+        public Object Model
+        {
+            get { return model; }
+            set { SetProperty(ref model, value); }
+        }
+
         public DelegateCommand<ObservableCollection<ImageCapture>> TakeSnapshotCommand { get; set; }
         protected async System.Threading.Tasks.Task TakeSnapshotAsync<T>(T list) where T : ObservableCollection<ImageCapture>
         {
-            CameraCaptureUI ccui = new CameraCaptureUI();
-            var file = await ccui.CaptureFileAsync(CameraCaptureUIMode.Photo);
-            if (file != null)
+            try
             {
-                list.Add(new ImageCapture { ImagePath = file.Path });
+                CameraCaptureUI ccui = new CameraCaptureUI();
+                var file = await ccui.CaptureFileAsync(CameraCaptureUIMode.Photo);
+                if (file != null)
+                {
+                    list.Add(new ImageCapture { ImagePath = file.Path });
+                }
             }
-        } 
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public DelegateCommand<ImageCapture> TakePictureCommand { get; set; }
     }
 }
--- Eqstra.VehicleInspection.UILogic/ViewModels/Passenger/VehicleDetailsUserControlViewModel.cs
+++ Eqstra.VehicleInspection.UILogic/ViewModels/Passenger/VehicleDetailsUserControlViewModel.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Eqstra.BusinessLogic.Passenger;
 
 namespace Eqstra.VehicleInspection.UILogic.ViewModels
 {
     public class VehicleDetailsUserControlViewModel : BaseViewModel
     {
+        public VehicleDetailsUserControlViewModel()
+        {
+            this.Model = new PVehicleDetails();
+        }
         public override void OnNavigatedTo(object navigationParameter, Windows.UI.Xaml.Navigation.NavigationMode navigationMode, Dictionary<string, object> viewModelState)
         {
             base.OnNavigatedTo(navigationParameter, navigationMode, viewModelState);
