d96bd85442fdd5f2852d4208dbb5fff5e27207b1
noor <syed.noor@mazikglobal.com>
2014-02-27 16:34:43 +0530

    ServiceScheduling App, UILogic projects added

---
+++ Eqstra.ServiceScheduling.UILogic/Services/AccountService.cs
@@ -0,0 +1,37 @@
+﻿using Eqstra.BusinessLogic;
+using Microsoft.Practices.Prism.StoreApps.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eqstra.ServiceScheduling.UILogic.Services
+{
+   public class AccountService : IAccountService
+    {
+       public const string SignedInUserKey = "AccountService_UserInfo";
+       ISessionStateService _sessionStateService;
+       ICredentialStore _credentialStore;
+       UserInfo _signedInUser;
+       public AccountService(ISessionStateService sessionStateService,ICredentialStore credentialStore)
+       {
+           _sessionStateService = sessionStateService;
+           _credentialStore = credentialStore;
+           if (_sessionStateService.SessionState.ContainsKey(SignedInUserKey))
+           {
+               _signedInUser = _sessionStateService.SessionState[SignedInUserKey] as UserInfo;
+           }
+          
+       }
+        public BusinessLogic.UserInfo SignedInUser
+        {
+            get { return _signedInUser; }
+        }
+
+        public void SignInAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Eqstra.ServiceScheduling/App.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Eqstra.BusinessLogic.Helpers;
+using Eqstra.ServiceScheduling.UILogic.Services;
+using Microsoft.Practices.Prism.PubSubEvents;
+using Microsoft.Practices.Prism.StoreApps;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace Eqstra.ServiceScheduling
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : MvvmAppBase
+    {
+
+        private readonly IUnityContainer _container = new UnityContainer();
+        public IEventAggregator EventAggregator { get; set; }
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+
+        }
+
+        protected override Task OnLaunchApplication(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Login", args.Arguments);
+            Window.Current.Activate();
+            return Task.FromResult<object>(null);
+        }
+
+        async protected override void OnInitialize(IActivatedEventArgs args)
+        {
+            base.OnInitialize(args);
+            EventAggregator = new EventAggregator();
+
+            var db = await ApplicationData.Current.RoamingFolder.TryGetItemAsync("SQLiteDB\\eqstraservicescheduling.sqlite") as StorageFile;
+            if (db == null)
+            {
+                var packDb = await Package.Current.InstalledLocation.GetFileAsync("SqliteDB\\eqstraservicescheduling.sqlite");
+                // var packDb = await sqliteDBFolder.GetFileAsync("eqstramobility.sqlite");
+                await packDb.CopyAsync(await ApplicationData.Current.RoamingFolder.CreateFolderAsync("SQLiteDB"));
+            }
+            SqliteHelper.Storage.ConnectionDatabaseAsync();
+            _container.RegisterInstance(NavigationService);
+            _container.RegisterInstance(EventAggregator);
+            _container.RegisterInstance(SessionStateService);
+
+            //_container.RegisterType<IAccountService, AccountService>(new ContainerControlledLifetimeManager());
+
+            //ViewModelLocator.Register(typeof(VehicleInspectionPage).ToString(), () => new VehicleInspectionPageViewModel());
+            ViewModelLocator.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewModelTypeName = string.Format(CultureInfo.InvariantCulture, "Eqstra.ServiceScheduling.UILogic.ViewModels.{0}ViewModel,Eqstra.ServiceScheduling.UILogic,Version 1.0.0.0, Culture=neutral", viewType.Name);
+                return Type.GetType(viewModelTypeName);
+            });
+            //ViewModelLocator.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            //    {
+            //        var viewModelTypeName = string.Format(CultureInfo.InvariantCulture, "Eqstra.VehicleInspection.UILogic.ViewModels.Passenger.{0}ViewModel,Eqstra.VehicleInspection.UILogic,Version 1.0.0.0, Culture=neutral", viewType.Name);
+            //        return Type.GetType(viewModelTypeName);
+            //    });
+        }
+
+        protected override object Resolve(Type type)
+        {
+            return _container.Resolve(type);
+        }
+
+        protected override IList<Windows.UI.ApplicationSettings.SettingsCommand> GetSettingsCommands()
+        {
+            var settingsCommands = new List<SettingsCommand>();
+            var accountService = _container.Resolve<IAccountService>();
+            if (accountService.SignedInUser != null)
+            {
+                //settingsCommands.Add(new SettingsCommand("resetpassword", "Reset Password", (handler) =>
+                //{
+                //    ResetPasswordPage page = new ResetPasswordPage();
+                //    page.Show();
+                //}));
+            }
+
+            settingsCommands.Add(new SettingsCommand("privacypolicy", "Privacy Policy", (handler) =>
+            {
+
+            }));
+            settingsCommands.Add(new SettingsCommand("help", "Help", (handler) =>
+            {
+
+            }));
+            // args.Request.ApplicationCommands.Add(command); 
+
+            return settingsCommands;
+        }
+    }
+}
