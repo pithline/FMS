d4c55d261c3be8997b02b918f8ecb7ab30bf7eee
noor <syed.noor@mazikglobal.com>
2014-03-10 12:26:08 +0530

    included the missed files

---
+++ Eqstra.ServiceScheduling.UILogic/ViewModels/ProfileUserControlViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Eqstra.ServiceScheduling.UILogic.Services;
+using Microsoft.Practices.Prism.StoreApps;
+using Microsoft.Practices.Prism.StoreApps.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Eqstra.ServiceScheduling.UILogic.ViewModels
+{
+    public class ProfileUserControlViewModel : ViewModel
+    {
+        INavigationService _navigationService;
+        IAccountService _accountService;
+        public ProfileUserControlViewModel(INavigationService navigationService,IAccountService accountService)
+        {
+            _navigationService = navigationService;
+            _accountService = accountService;
+
+            LogoutCommand = new DelegateCommand(() =>
+            {
+                _accountService.SignOut();
+                _navigationService.Navigate("Login", null);
+                _navigationService.ClearHistory();
+            });
+        }
+
+        public ICommand LogoutCommand { get; private set; }
+    }
+}
---
+++ Eqstra.VehicleInspection.UILogic/Services/IdentityServiceProxy.cs
@@ -0,0 +1,39 @@
+﻿using Eqstra.BusinessLogic;
+using Eqstra.BusinessLogic.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eqstra.VehicleInspection.UILogic.Services
+{
+   public class IdentityServiceProxy : IIdentityService
+    {
+        async public Task<Tuple<LogonResult,string>> LogonAsync(string userId, string password)
+        {
+            var isUserExists = (await SqliteHelper.Storage.LoadTableAsync<LoggedInUser>()).Any(x => x.UserId == userId);
+            if (isUserExists)
+            {
+                var result = await SqliteHelper.Storage.GetSingleRecordAsync<LoggedInUser>(x => (x.UserId == userId && x.Password == password));
+                if (result != null)
+                {
+                    return new Tuple<LogonResult, string>(new LogonResult { UserInfo = new UserInfo { UserId = result.UserId } }, "");
+                }
+                else
+                {
+                    return new Tuple<LogonResult, string>(null, "Whoa! The entered password is incorrect, please verify the password you entered.");
+                } 
+            }
+            else
+            {
+                return new Tuple<LogonResult, string>(null, "Whoa! No such user exists, verfiy if the entered userid is correct.");
+            }
+        }
+
+        public Task<bool> VerifyAcitveSessionAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
