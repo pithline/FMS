09c128875a18b10605fa52a9eb5132c0e7f11cf3
noor <noor9saleem@gmail.com>
2014-03-10 18:16:14 +0530

    BackgroundTask proj added

---
+++ Eqstra.VehicleInspection.BackgroundTask/SilentSync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace Eqstra.VehicleInspection.BackgroundTask
+{
+    public sealed class SilentSync : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
