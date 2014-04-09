9f72f52a3a7535f65df54ab67030ecf639581552
noor <noor9saleem@gmail.com>
2014-04-09 20:26:29 +0530

    Doc Briefing usercontrol

---
+++ Eqstra.DocumentDelivery/Views/BriefDetailsUserControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Eqstra.DocumentDelivery.Views
+{
+    public sealed partial class BriefDetailsUserControl : UserControl
+    {
+        public List<DocBrief> DocumentList { get; set; }
+        
+        public BriefDetailsUserControl()
+        {
+            this.InitializeComponent();
+            this.DocumentList = new List<DocBrief>
+            {
+                new DocBrief{CaseNumber="E4323",DocumentType = "LicenseDisc"},
+                new DocBrief{CaseNumber="E4323",DocumentType = "LicenseDisc"},
+                new DocBrief{CaseNumber="E4323",DocumentType = "LicenseDisc"},
+                new DocBrief{CaseNumber="E4323",DocumentType = "LicenseDisc"},
+                new DocBrief{CaseNumber="E4323",DocumentType = "LicenseDisc"},
+                new DocBrief{CaseNumber="E4323",DocumentType = "LicenseDisc"},
+                new DocBrief{CaseNumber="E4323",DocumentType = "LicenseDisc"},
+                new DocBrief{CaseNumber="E4323",DocumentType = "LicenseDisc"},
+                new DocBrief{CaseNumber="E4323",DocumentType = "LicenseDisc"},
+                new DocBrief{CaseNumber="E4323",DocumentType = "LicenseDisc"},
+            };
+        }
+    }
+
+
+    public class DocBrief
+    {
+        public string CaseNumber { get; set; }
+        public string DocumentType { get; set; }
+    }
+}
