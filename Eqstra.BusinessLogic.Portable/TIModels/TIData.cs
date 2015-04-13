a94ae1876f063f36911015c62d833cd6fda0ec8f
kasif <kasifnawaz@outlook.com>
2015-04-13 19:47:57 +0530

    user profile

---
+++ Eqstra.BusinessLogic.Portable/TIModels/TIData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace Eqstra.BusinessLogic.Portable.TIModels
+{
+    public class TIData
+    {
+        private string causeOfDamage;
+
+        public string CauseOfDamage
+        {
+            get { return causeOfDamage; }
+            set { causeOfDamage = value; }
+        }
+
+        private string remedy;
+
+        public string Remedy
+        {
+            get { return remedy; }
+            set { remedy = value; }
+        }
+
+        private string recommendation;
+
+        public string Recommendation
+        {
+            get { return recommendation; }
+            set { recommendation = value; }
+        }
+
+        private DateTime completionDate;
+
+        public DateTime CompletionDate
+        {
+            get { return completionDate; }
+            set { completionDate = value; }
+        }
+        private long caseServiceRecID;
+
+        public long CaseServiceRecID
+        {
+            get { return caseServiceRecID; }
+            set { caseServiceRecID = value; }
+        }
+
+        private ObservableCollection<object> caseCategoryAuthList;
+
+        public ObservableCollection<object> CaseCategoryAuthList
+        {
+            get { return caseCategoryAuthList; }
+            set { caseCategoryAuthList = value; }
+        }
+
+        private bool shouldSave;
+
+        public bool ShouldSave
+        {
+            get { return shouldSave; }
+            set { shouldSave = value; }
+        }
+    }
+}
