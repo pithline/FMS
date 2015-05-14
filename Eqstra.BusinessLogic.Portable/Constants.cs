41e16d63f814c37ed6ac14faa1acd555b0686af4
noor <syed.noor@outlook.com>
2015-05-14 13:42:08 +0530

    TI wp : image sync fixed

--- Eqstra.BusinessLogic.Portable/Constants.cs
+++ Eqstra.BusinessLogic.Portable/Constants.cs
@@ -11,11 +11,11 @@ namespace Eqstra.BusinessLogic.Portable
         public const string USERINFO = "UserInfo";
         public const string SELECTEDTASK = "SelectedTask";
         public const string SSDCAMERACAPTURE = "SSDCameraCapture";
-        
-        public const string APIURL = "https://eqstraapi.mazikglobal.com/api/da";
-        //public const string APIURL = "http://192.168.1.10/Eqstra.CrossPlatform.API/api/da";
-        //public const string TOKENURL = "http://192.168.1.10/Eqstra.CrossPlatform.API/token";
-        public const string TOKENURL = "https://eqstraapi.mazikglobal.com/token";
+
+        public const string APIURL = "http://192.168.1.2/Eqstra.CrossPlatform.API/api/da";
+        public const string TOKENURL = "http://192.168.1.2/Eqstra.CrossPlatform.API/token";
+        //public const string APIURL = "https://eqstraapi.mazikglobal.com/api/da";
+        //public const string TOKENURL = "https://eqstraapi.mazikglobal.com/token";
         
         public const string ACCESSTOKEN = "AccessToken";
     }
